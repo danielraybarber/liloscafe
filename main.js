6ab104d7b48687c3e08ee22525927a48ab831b1a
Daniel Barber <Daniel@Daniels-MacBook-Pro.local>
2020-04-04 18:36:20 -0500

    full project commiting over

---
+++ main.js
@@ -0,0 +1,9 @@
+function handleSubmit() {
+  //takw the name of the user
+  //display an alert, thanking user
+  var firstNameInput = document.getElementById("fname");
+  var firstName = firstNameInput.value;
+  alert(
+    "Thank you for your feedback. If you want the owner to get this please email him at danielraybarber@outlook.com"
+  );
+}
